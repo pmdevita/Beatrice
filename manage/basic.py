16ef3074c73fa6275389e5902162959ccd925ac7
Peter DeVita <mewtwo2643@yahoo.com>
2022-02-25 02:51:12 -0500

    Add experimental voice channel audio file playing

--- manage/basic.py
+++ manage/basic.py
@@ -1,11 +1,22 @@
+import asyncio
+
+import nextcord
 import nextcord.ext.commands as commands
+from nextcord import FFmpegOpusAudio
 from random import choice
 from util import find_mentions, member_to_mention
+
+HI_FILES = [
+    ("beatrice_hi1.opus", "You come in here without knocking? What a rude one you are."),
+    ("beatrice_hi2.opus", "You're irritating me to death. Either stop it or get blown away."),
+    ("beatrice_hi3.opus", "You come in here every day without even knocking. You truly have no manners whatsoever.")
+]
 
 
 class Basic(commands.Cog):
     def __init__(self, discord):
         self.discord = discord
+        self.connection = None
 
     @commands.command("hi", aliases=["hello", "hey", "howdy", "beatrice", "beako", "betty"])
     async def hello(self, ctx: commands.Context, *args):
@@ -14,8 +25,13 @@ class Basic(commands.Cog):
         if mentions:
             member = mentions[0]
 
-        templates = ["Hmmmmm... hi {}.", "Yes, yes, hello {}.", "Hi {}, I guess.", "I'm busy right now {}, shoo, shoo!"]
-        await ctx.send(choice(templates).format(member.display_name))
+        if member.voice is None:
+            templates = ["Hmmmmm... hi {}.", "Yes, yes, hello {}.", "Hi {}, I guess.", "I'm busy right now {}, shoo, shoo!"]
+            await ctx.send(choice(templates).format(member.display_name))
+        else:
+            line = choice(HI_FILES)
+            await ctx.send(line[1].format(member.display_name))
+            await self.soundboard(member, f"assets/{line[0]}")
 
     @commands.command(name="ping")
     async def ping(self, ctx: commands.Context):
@@ -34,6 +50,25 @@ class Basic(commands.Cog):
             await ctx.send(text)
         else:
             await ctx.send(f"Who?")
+
+    @commands.command("inhale")
+    async def inhale_a_car(self, ctx: commands.Context, *args):
+        await self.soundboard(ctx.author, "assets/inhale_a_car.opus")
+
+    @commands.command("mouthful")
+    async def mouthful(self, ctx: commands.Context, *args):
+        await self.soundboard(ctx.author, "assets/mouthful_mode.opus")
+
+    async def soundboard(self, member: nextcord.Member, file_path):
+        if member.voice is None:
+            return
+
+        connection = await member.voice.channel.connect()
+        connection.play(FFmpegOpusAudio(file_path))
+        while connection.is_playing():
+            await asyncio.sleep(1)
+        await connection.disconnect()
+
 
     # @commands.Cog.listener("on_message")
     # async def on_message(self, message: nextcord.Message, *arg):
