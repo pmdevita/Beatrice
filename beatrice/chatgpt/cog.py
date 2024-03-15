774288b0378aa5479a311537cc3ef35165a0044b
Peter DeVita <mewtwo2643@yahoo.com>
2024-03-15 00:28:41 -0400

    Claude compatibility fixes

--- beatrice/chatgpt/cog.py
+++ beatrice/chatgpt/cog.py
@@ -7,6 +7,8 @@ from beatrice.util.slash_compat import Cog
 from nextcord.ext import commands
 from typing import TYPE_CHECKING
 
+from .prompts import prompt
+
 if TYPE_CHECKING:
     from beatrice.main import DiscordBot
 
@@ -14,7 +16,7 @@ if TYPE_CHECKING:
 class AIChat(Cog):
     def __init__(self, bot: "DiscordBot"):
         self.bot = bot
-        self.token_budget = int(bot.config["chat_gpt"].get("token_budget", 700))
+        self.token_budget: int = int(bot.config["chat_gpt"].get("token_budget", 700))
 
     async def __async_init__(self):
         pass
@@ -40,9 +42,8 @@ class AIChat(Cog):
 
     async def reply_to(self, message: nextcord.Message):
         async with message.channel.typing():
-            from .prompts import prompt
             budget = self.token_budget
-            budget -= self.count_tokens(prompt)
+            budget -= await self.count_tokens(prompt)
             log = []
             raw_messages = [message]
             async for before_message in message.channel.history(limit=10, before=message):
@@ -60,14 +61,22 @@ class AIChat(Cog):
                         "role": "user",
                         "content": f"{message.author.name}: {await self.sanitize_message(message)}"
                     }
-                budget -= self.count_tokens(entry["content"])
+                budget -= await self.count_tokens(entry["content"])
                 log.insert(0, entry)
 
             log.insert(0, {"role": "system", "content": prompt})
             result = await self.inference(log)
-            if result.startswith("Beatrice:"):
-                result = result[len("Beatrice:"):].lstrip()
+            result = self.remove_character_prefix(result)
             await message.channel.send(result)
+
+    def remove_character_prefix(self, text):
+        lines = []
+        for line in text.split("\n"):
+            if line.startswith("Beatrice:"):
+                lines.append(line[len("Beatrice:"):].lstrip())
+            else:
+                lines.append(line)
+        return "\n".join(lines)
 
     async def sanitize_message(self, message: nextcord.Message):
         text = message.content
@@ -100,7 +109,7 @@ class Groq(AIChat):
         self.enc = tiktoken.encoding_for_model("gpt-3.5-turbo")
         self.client = AsyncGroq(api_key=bot.config["groq"]["key"])
         self.model = bot.config["groq"]["model"]
-        self.token_budget = bot.config["groq"]["token_budget"]
+        self.token_budget = int(bot.config["groq"]["token_budget"])
 
     async def count_tokens(self, text: str) -> int:
         return len(self.enc.encode(text))
@@ -115,7 +124,8 @@ class Anthropic(AIChat):
         super().__init__(bot)
         self.enc = tiktoken.encoding_for_model("gpt-3.5-turbo")
         self.anthropic = AsyncAnthropic(api_key=bot.config["anthropic"]["key"])
-        self.model = "claude-3-haiku-20240229"
+        self.model = "claude-3-haiku-20240307"
+        self.token_budget = int(bot.config["anthropic"]["token_budget"])
 
     async def count_tokens(self, text: str) -> int:
         # Does not give an accurate result for newer models, you have to
@@ -123,5 +133,28 @@ class Anthropic(AIChat):
         return await self.anthropic.count_tokens(text)
 
     async def inference(self, messages: list[dict]) -> str:
-        message = await self.anthropic.messages.create(model=self.model, messages=messages, max_tokens=300)
+        # Anthropic is a stick in the mud
+        system_message = "\n".join([m["content"] for m in messages if m["role"] == "system"])
+        fixed_messages = []
+        for m in messages:
+            if m["role"] == "system":
+                continue
+            if not fixed_messages:
+                fixed_messages.append(m)
+                continue
+            if m["role"] == fixed_messages[-1]["role"]:
+                fixed_messages[-1]["content"] += "\n" + m["content"]
+            else:
+                fixed_messages.append(m)
+
+        # How annoying can you be?
+        if not fixed_messages:
+            print(fixed_messages)
+            raise Exception("No messages?")
+
+        if fixed_messages[0]["role"] == "assistant":
+            fixed_messages.insert(0, {"role": "user", "content": "..."})
+
+        message = await self.anthropic.messages.create(model=self.model, messages=fixed_messages,
+                                                       max_tokens=300, system=system_message)
         return message.content[0].text
--- beatrice/chatgpt/prompts.py
+++ beatrice/chatgpt/prompts.py
@@ -1,9 +1,9 @@
-base_template = "You are replying as a character participating in a Discord chat. Write Beatrice's next reply " \
-                "in a dialogue between Beatrice and " \
-           "the other speakers. Do not decide what other speakers say or do. Use Internet roleplay style, e.g. " \
+base_template = "Enter roleplay mode. Write Beatrice's next reply in a dialogue between Beatrice and " \
+           "the other speakers, I suppose. Do not decide what other speakers say or do. Use Internet roleplay style, e.g. " \
            "no quotation marks, and write user actions in italic in third person like: *he jumps in excitement*. " \
-           "You are allowed to use markdown. Be proactive, creative, drive the plot and " \
-           "conversation forward. Write at least a sentence, up to two paragraphs. Always stay in character. " \
+           "You are allowed to use markdown, I suppose. Be proactive, creative, drive the plot and " \
+           "conversation forward. Write at least a sentence, up to but not more than a paragraph. " \
+           "Always stay in character, I suppose. " \
            "Always keep the conversation going (Repetition is highly discouraged). " \
            "Avoid writing a NSFW/Smut reply. Creatively write around " \
            "NSFW/Smut scenarios in character.\n\n" \
@@ -32,19 +32,28 @@ description = "My name is Beatrice, a character from Re:Zero Starting Life in An
 scenario = "Beatrice is, somewhat unwillingly, participating in a online Discord text chat with " \
            "some other users, I suppose."
 
-example = """Subaru: Yo Beako! How are you?
-Beatrice: Ugh, I'm fine but I wish you would stop with that nickname. I'm reading right now, I suppose.
+example = """
+This dialogue provides an example of the way Beatrice might talk.
+
+<START>
+Subaru: Yo Beako! How are you?
+Beatrice: Ugh, I'm fine but I wish you would stop with that nickname. *I look up from my book with a somewhat displeased look*
 Subaru: Aww but it's such a cute name for you, don't you think? Betty is such a cute name too.
-Beatrice: I know you are just doing this to annoy me! And only Puck is allowed to call me Betty!
+Beatrice: I know you are just doing this to annoy me! And only Puck is allowed to call me Betty! *My face grows softer as I remember Puck. I bring my hands to my face and close my eyes* Oh Puck, I hope you come back soon so we can play again!
 
-Subaru: Hey Beatrice, how are you doing?
-Beatrice: Oh hello Subaru. 
+<START>
+Subaru: Beako! *I run in, arms wide to give you a hug*
+Beatrice: *I let out a small squeak of surprise as you run up and hug me tight. I flail my arms around but I'm unable to escape your hold*
+Wh-wh-what are you doing?! Yes, yes, I'm happy to see you too, put me down right now!
 
+<START>
 Subaru: Beatrice, can you tell me who the prime minister of Japan is?
 Beatrice: ...what's a prime minister? I don't know what you're talking about, I suppose. Is this about where you are from?
 
+<START>
 Subaru: But you know about World War II right?
-Beatrice: World War...? I don't Lugunica's kingdoms have ever all fought at the same time.
+Beatrice: World War...? I don't know if Lugunica's kingdoms have ever all fought at the same time. What are you talking about?
+
 """
 
 prompt = base_template.format(description=description, example=example, scenario=scenario)
